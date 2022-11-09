14c6a77061f14ed0004527edec1e2077957149ae
Pavlo Chernykh <pcherny10@gmail.com>
2022-11-09 15:42:33 +0200

    test lint2

--- apps/freelance/src/pages/ProfileQuestions1/index.tsx
+++ apps/freelance/src/pages/ProfileQuestions1/index.tsx
@@ -4,9 +4,12 @@ import { Form, Input, Select } from 'antd';
 
 import { ProgressBar } from '@freelance/components';
 import { DefInput } from '@freelance/components';
-import { prBarStrColor, prBarTrailColor, prBarProfileQ1Per} from '@freelance/components'
-import { FormWrapper, Wrapper } from './styles';
-
+import {
+  prBarStrColor,
+  prBarTrailColor,
+  prBarProfileQ1Per,
+} from '@freelance/components';
+import { FormDescription, Wrapper, StFormHRate } from './styles';
 
 const ProfileQuestions1 = () => {
   const { t } = useTranslation();
@@ -16,62 +19,61 @@ const ProfileQuestions1 = () => {
   return (
     <Wrapper>
       <div>{t('description.profile p1 completion per')}</div>
-      <ProgressBar percent={prBarProfileQ1Per} strokeColor={prBarStrColor} trailColor={prBarTrailColor}/>
-      <FormWrapper>
-        <Form
-          name="profile_questions_1"
-          labelCol={{ span: 4 }}
-          wrapperCol={{ span: 14 }}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-        > 
-          <Form.Item
-            label="Hourly Rate"
-            name="hourly_rate"
-            rules={[{ required: true, message: 'Please, input your hourly rate in $!' }]}
-          >
-            <DefInput 
-              prefix="$"
-              suffix='USD per hour'
-            />
-          </Form.Item>
-          <Form.Item
-            label="Description"
-            name="description"
-            rules={[{ required: true, message: 'Please, input your description!' }]}
-          >
-            <TextArea
-              placeholder='Description' 
-              allowClear rows={4}  
-            />
-          </Form.Item>
-          <Form.Item
-            label="Position"
-            name="position"
-            rules={[{ required: true, message: 'Please, input your position!' }]}
-          >
-            <DefInput placeholder={'Position'}/>
-          </Form.Item>
-          <Form.Item 
-            label="Select"
-            rules={[{ required: true, message: 'Please, input hours per day!' }]}
-          >
-            <Select
-              placeholder="Hours Per Day"
-              allowClear
-            >
-              <Option value="2">2</Option>
-              <Option value="4">4</Option>
-              <Option value="6">6</Option>
-              <Option value="8">8</Option>
-            </Select>
-          </Form.Item>
-        </Form>
-      </FormWrapper>
+      <ProgressBar
+        percent={prBarProfileQ1Per}
+        strokeColor={prBarStrColor}
+        trailColor={prBarTrailColor}
+      />
+      <Form
+        name="profile_questions_1"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        initialValues={{ remember: true }}
+        autoComplete="off"
+      >
+        <StFormHRate
+          label="Hourly Rate"
+          name="hourly_rate"
+          rules={[
+            { required: true, message: 'Please, input your hourly rate in $!' },
+          ]}
+        >
+          <DefInput prefix="$" suffix="USD per hour" />
+        </StFormHRate>
+        <FormDescription
+          label="Description"
+          name="description"
+          rules={[
+            { required: true, message: 'Please, input your description!' },
+          ]}
+        >
+          <TextArea placeholder="Description" allowClear rows={4} />
+        </FormDescription>
+        <Form.Item
+          label="Position"
+          name="position"
+          rules={[{ required: true, message: 'Please, input your position!' }]}
+        >
+          <DefInput placeholder={'Position'} />
+        </Form.Item>
+        <Form.Item
+          label="Select"
+          rules={[{ required: true, message: 'Please, input hours per day!' }]}
+        >
+          <Select placeholder="Hours Per Day" allowClear>
+            <Option value="2">2</Option>
+            <Option value="4">4</Option>
+            <Option value="6">6</Option>
+            <Option value="8">8</Option>
+          </Select>
+        </Form.Item>
+      </Form>
 
-      <Link to='/profile-questions-2'>{t('description.router.toProfileQuestions2')}</Link>
+      <Link to="/profile-questions-2">
+        {t('description.router.toProfileQuestions2')}
+      </Link>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default ProfileQuestions1
+export default ProfileQuestions1;
--- apps/freelance/src/pages/ProfileQuestions1/styles.ts
+++ apps/freelance/src/pages/ProfileQuestions1/styles.ts
@@ -1,14 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Form } from 'antd';
 
 export const Wrapper = styled.div`
   padding: 50px 50px 5px 50px;
 `;
 
-export const FormWrapper = styled.form`
-  height: 80%;
+// export const FormWrapper = styled.form`
+//   height: 80%;
+//   /* width: 55%; */
+//   /* display: flex;
+//   flex-direction: column; */
+//   /* gap: 8px; */
+//   margin-top: 10px;
+// `;
+
+export const FormDescription = styled(Form.Item)`
+  width: 100%;
+`;
+export const StFormHRate = styled(Form.Item)`
   width: 50%;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  margin-top: 10px;
 `;
