592f2986e1abbcc8c613e0f30a429ba5126544c3
MykhailoKutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-11-11 00:03:53 +0200

    add GlobalStyle

--- apps/freelance/src/main.tsx
+++ apps/freelance/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode, Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import './translations';
 
 import App from './app/app';
 import { store } from './redux/store';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { baseTheme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -15,9 +18,12 @@ root.render(
   <StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <ThemeProvider theme={baseTheme}>
+          <BrowserRouter>
+            <GlobalStyle />
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
       </Provider>
     </Suspense>
   </StrictMode>,
