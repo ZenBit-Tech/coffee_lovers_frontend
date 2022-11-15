0d1b2c9e29852816c1bb88b3e23a2388eb721942
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2022-11-15 21:11:41 +0200

    fix: styles in page

--- apps/freelance/src/pages/JobOwnerDashboard/styles.ts
+++ apps/freelance/src/pages/JobOwnerDashboard/styles.ts
@@ -1,22 +1,5 @@
-import { baseTheme } from 'src/styles/theme';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   padding: 1rem;
-`;
-
-export const UploadField = styled.div`
-  width: 5rem;
-  height: 5rem;
-  border-radius: 50%;
-  background-color: ${baseTheme.colors.bg};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    cursor: pointer;
-    border: 1px ${baseTheme.colors.primary} dashed;
-  }
 `;
