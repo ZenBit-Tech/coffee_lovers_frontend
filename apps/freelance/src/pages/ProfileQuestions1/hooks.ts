4e78c2a0b57959d6437880e90aa5d3a0d24f538f
Pavlo Chernykh <pcherny10@gmail.com>
2022-11-15 13:03:32 +0200

    fixed mistakes

--- apps/freelance/src/pages/ProfileQuestions1/hooks.ts
+++ apps/freelance/src/pages/ProfileQuestions1/hooks.ts
@@ -3,17 +3,13 @@ import { profileQ1Payload } from 'redux/services/types/profileQuestions1.types';
 import { IProfileQuestions1 } from './model';
 
 export const onFinishLogic = (values: IProfileQuestions1): profileQ1Payload => {
-  const jsonedEdu = JSON.parse(JSON.stringify(values.education));
-  const jsonedWork = JSON.parse(JSON.stringify(values.work_history_wrapper));
-  const returnedValues = {
+  return {
     ...values,
-    education_descr: jsonedEdu.information_about_education,
-    education_from: jsonedEdu.education_from.substring(0, 4),
-    education_to: jsonedEdu.education_to.substring(0, 4),
-    work_history_descr: jsonedWork.work_history,
-    work_history_from: jsonedWork.work_from.substring(0, 4),
-    work_history_to: jsonedWork.work_to.substring(0, 4),
+    education_descr: values.education.information_about_education,
+    education_from: values.education.education_from.format('YYYY'),
+    education_to: values.education.education_to.format('YYYY'),
+    work_history_descr: values.work_history_wrapper.work_history,
+    work_history_from: values.work_history_wrapper.work_from.format('YYYY'),
+    work_history_to: values.work_history_wrapper.work_to.format('YYYY'),
   };
-
-  return returnedValues;
 };
