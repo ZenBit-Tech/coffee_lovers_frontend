4d7e8c4972865150d4f5fa3a2bfae9d2deba5f8b
SergKukh <kukharchuk.serg@gmail.com>
2022-11-22 20:10:35 +0200

    fix: find job wrapper styles

--- apps/freelance/src/pages/FindJobs/styles.ts
+++ apps/freelance/src/pages/FindJobs/styles.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 10px;
+  padding-top: 10px;
 `;
 
 export const PageBar = styled.div`
