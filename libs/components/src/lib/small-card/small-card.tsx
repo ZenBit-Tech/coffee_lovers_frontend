91cc6683e2265a7c3e61f09637bfe86a0c75f735
Kashyrets Ivanna <kasirecivanna@gmail.com>
2022-11-28 20:28:53 +0200

    fix: eslint padding-line-between

--- libs/components/src/lib/small-card/small-card.tsx
+++ libs/components/src/lib/small-card/small-card.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 
 import { SmallCardProps } from './model';
 import { StyledSmallCard } from './styles';
+
 export function SmallCard(props: SmallCardProps): ReactElement {
   return <StyledSmallCard disabled>{props.text}</StyledSmallCard>;
 }
