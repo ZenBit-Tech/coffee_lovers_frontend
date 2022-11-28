d73bff6f8ddd3719517db2fd57af0009dd956a03
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2022-11-28 20:23:43 +0200

    fix: getHeaders return type

--- apps/freelance/src/utils/api.ts
+++ apps/freelance/src/utils/api.ts
@@ -2,7 +2,10 @@ import { BaseQueryApi } from '@reduxjs/toolkit/dist/query';
 import { MaybePromise } from '@reduxjs/toolkit/dist/query/tsHelpers';
 import { RootState } from 'redux/store';
 
-export const getHeaders = () => {
+export const getHeaders = (): ((
+  headers: Headers,
+  api: Pick<BaseQueryApi, 'getState'>,
+) => MaybePromise<void | Headers> | undefined) => {
   return (
     headers: Headers,
     api: Pick<BaseQueryApi, 'getState'>,
