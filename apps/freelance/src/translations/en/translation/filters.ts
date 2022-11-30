329b747f17560ef283d57d0c6e845521f9420d4b
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2022-11-30 16:57:40 +0200

    refactored translations

---
+++ apps/freelance/src/translations/en/translation/filters.ts
@@ -0,0 +1,21 @@
+export default {
+  filters: {
+    title: 'Filters',
+    category: 'Category',
+    category_placeholder: 'Start typing to search categories',
+    skills: 'Skills',
+    skills_placeholder: 'Start typing to search skills',
+    hrly_rate: 'Hourly rate',
+    time: 'Time available',
+    time_placeholder: 'Hrs per day',
+    english_level: 'English level',
+    english_level_placeholder: 'My level is',
+    buttons: {
+      clear: 'Clear',
+      apply: 'Apply',
+    },
+    errors: {
+      selectMax: 'Maximum 6 items',
+    },
+  },
+};
