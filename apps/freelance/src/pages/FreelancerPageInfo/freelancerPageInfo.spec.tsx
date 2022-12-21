29c8efa284869ba96de33f3440419cff9bdba70b
Pavlo Chernykh <pcherny10@gmail.com>
2022-12-21 10:22:31 +0200

    fixes

--- apps/freelance/src/pages/FreelancerPageInfo/freelancerPageInfo.spec.tsx
+++ apps/freelance/src/pages/FreelancerPageInfo/freelancerPageInfo.spec.tsx
@@ -10,7 +10,7 @@ jest.mock('redux/services/freelancers', () => ({
   useGetFreelancerByIdQuery: () => ({ data: mockUserData, isLoading: false }),
 }));
 
-describe('FreelancerPageInfo rendering component', () => {
+describe('FreelancerPageInfo rendering component using data from rtk query', () => {
   const container: Element = document.createElement('div');
 
   beforeAll(() => {
@@ -44,63 +44,39 @@ describe('FreelancerPageInfo rendering component', () => {
     expect(baseElement).toBeTruthy();
   });
 
-  it('field with freelancer name and surname displays correct data taken from useGetFreelancerByIdQuery hook', () => {
+  it('field with freelancer data displays correct data from rtk query', () => {
     render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.nameSurname).textContent).toBe(
       `${mockUserData.first_name} ${mockUserData.last_name}`,
     );
-  });
-
-  it('field with freelancer category name displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(
       screen.getByTestId(freelancerPageInfo.categoryName).textContent,
     ).toBe(`${mockUserData.category.name}`);
-  });
-
-  it('field with freelancer hourly rate displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.hr).textContent).toBe(
       `${mockUserData.hourly_rate} $`,
     );
-  });
-
-  it('field with freelancer description displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.desc).textContent).toBe(
       `${mockUserData.description}`,
     );
-  });
-
-  it('field with freelancer position displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.pos).textContent).toBe(
       `${mockUserData.position}`,
     );
-  });
-
-  it('field with freelancer available time displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.avtime).textContent).toBe(
       `${mockUserData.available_time}`,
     );
-  });
-
-  it('field with freelancer english level displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(screen.getByTestId(freelancerPageInfo.englevel).textContent).toBe(
       `${mockUserData.english_level}`,
     );
   });
 
-  it('fields with freelancer education displays correct data taken from useGetFreelancerByIdQuery hook', () => {
+  it('fields with mapped array of objects displays correct data', () => {
     render(<FreelancerPageInfo />);
 
     expect(mockUserData.educations).toEqual(
@@ -108,64 +84,54 @@ describe('FreelancerPageInfo rendering component', () => {
         expect.objectContaining({ education_from: '2010' }),
       ]),
     );
+
     expect(mockUserData.educations).toEqual(
       expect.arrayContaining([expect.objectContaining({ id: 2 })]),
     );
+
     expect(mockUserData.educations).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ education_descr: 'Harward IU' }),
       ]),
     );
-  });
-
-  it('fields with freelancer education are mapped correctly', () => {
-    render(<FreelancerPageInfo />);
-
-    const educations = screen.getAllByTestId(freelancerPageInfo.edu);
-    expect(educations.length).toBe(2);
-  });
-
-  it('fields with freelancer workhistory displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(mockUserData.workHistory).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ work_history_from: '2016' }),
       ]),
     );
+
     expect(mockUserData.workHistory).toEqual(
       expect.arrayContaining([expect.objectContaining({ id: 3 })]),
     );
+
     expect(mockUserData.workHistory).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ work_history_descr: 'Boudin Bakery' }),
       ]),
     );
-  });
-
-  it('fields with freelancer workhistory are mapped correctly', () => {
-    render(<FreelancerPageInfo />);
-
-    const workhistory = screen.getAllByTestId(freelancerPageInfo.work);
-    expect(workhistory.length).toBe(3);
-  });
-
-  it('fields with freelancer skills displays correct data taken from useGetFreelancerByIdQuery hook', () => {
-    render(<FreelancerPageInfo />);
 
     expect(mockUserData.skills).toEqual(
       expect.arrayContaining([expect.objectContaining({ name: 'HTML' })]),
     );
+
     expect(mockUserData.skills).toEqual(
       expect.arrayContaining([expect.objectContaining({ id: 2 })]),
     );
+
     expect(mockUserData.skills).not.toEqual(
       expect.arrayContaining([expect.objectContaining({ id: 'not real id' })]),
     );
   });
 
-  it('fields with freelancer skills are mapped correctly', () => {
+  it('fields with freelancer education, work and skills are mapped correctly', () => {
     render(<FreelancerPageInfo />);
+
+    const educations = screen.getAllByTestId(freelancerPageInfo.edu);
+    expect(educations.length).toBe(2);
+
+    const workhistory = screen.getAllByTestId(freelancerPageInfo.work);
+    expect(workhistory.length).toBe(3);
 
     const skills = screen.getAllByTestId(freelancerPageInfo.skills);
     expect(skills.length).toBe(3);
