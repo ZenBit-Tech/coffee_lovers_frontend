d7122a50c8513fe91b202ee290b5bf19ca652674
Pavlo Chernykh <pcherny10@gmail.com>
2022-12-28 16:39:47 +0200

    fix

--- libs/components/src/lib/UI/notification/index.tsx
+++ libs/components/src/lib/UI/notification/index.tsx
@@ -1,8 +1,6 @@
 import { ReactElement } from 'react';
 import { notification } from 'antd';
 
-// export type NotificationType = 'success' | 'info' | 'warning' | 'error';
-
 export enum NotificationType {
   SUCCESS = 'success',
   INFO = 'info',
