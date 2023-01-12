7207cc74453fcb40430aeff21b78fe1e94aff87d
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2023-01-12 17:19:25 +0200

    fix: refetch tags & chat route in hires list

--- libs/components/src/lib/hired-card/index.tsx
+++ libs/components/src/lib/hired-card/index.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
 import { t } from 'i18next';
-import { Avatar, DangerButton, SecondaryButton } from '@freelance/components';
+import { generatePath, useNavigate } from 'react-router-dom';
+import {
+  Avatar,
+  DangerButton,
+  routes,
+  SecondaryButton,
+} from '@freelance/components';
 import { Contract, ContractStatus } from 'src/redux/types/contracts.types';
+import { Job } from 'src/redux/types/jobs.types';
 import { User } from 'src/redux/types/user.types';
 import { getFileUrl } from 'src/utils/api';
 
@@ -20,10 +27,16 @@ import useHiredCard from './useHiredCard';
 interface HiredCardProps {
   freelancer: User;
   contract: Contract;
+  job?: Job;
 }
 
-export const HiredCard: FC<HiredCardProps> = ({ freelancer, contract }) => {
+export const HiredCard: FC<HiredCardProps> = ({
+  freelancer,
+  contract,
+  job,
+}) => {
   const { closeContractHandler } = useHiredCard(contract, freelancer);
+  const navigate = useNavigate();
 
   return (
     <Wrapper>
@@ -46,7 +59,18 @@ export const HiredCard: FC<HiredCardProps> = ({ freelancer, contract }) => {
         </StyledStatus>
         {contract.status === ContractStatus.ACTIVE && (
           <>
-            <SecondaryButton>{t('postedJobDetails.btns.chat')}</SecondaryButton>
+            <SecondaryButton
+              onClick={() =>
+                navigate(
+                  generatePath(routes.chatUser, {
+                    userId: freelancer.id,
+                    jobId: job?.id,
+                  }),
+                )
+              }
+            >
+              {t('postedJobDetails.btns.chat')}
+            </SecondaryButton>
             <DangerButton onClick={closeContractHandler}>
               {t('postedJobDetails.btns.closeContract')}
             </DangerButton>
