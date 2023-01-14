ee57104e978059adbf427aa0bbfaefd9c6250d15
Kashyrets Ivanna <kasirecivanna@gmail.com>
2023-01-14 19:25:51 +0200

    fix: styles

--- libs/components/src/lib/google-login-button/styles.ts
+++ libs/components/src/lib/google-login-button/styles.ts
@@ -7,7 +7,6 @@ export const ButtonContainer = styled.div`
 
 export const GoogleButton = styled.button`
   width: 100%;
-  height: 100%;
   background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/1200px-Google_Chrome_icon_%28February_2022%29.svg.png);
   background-size: contain;
   background-position: left;
