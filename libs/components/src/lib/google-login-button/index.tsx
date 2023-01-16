a073869e09e6aee8b8c68139e32921c757fc23f6
Kashyrets Ivanna <kasirecivanna@gmail.com>
2023-01-16 16:43:12 +0200

    fix:discard some changes

--- libs/components/src/lib/google-login-button/index.tsx
+++ libs/components/src/lib/google-login-button/index.tsx
@@ -1,20 +1,19 @@
 import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { routes } from '@freelance/constants';
-import { useGoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import { setUser } from 'src/redux/auth/auth-slice';
 import { useAddUserGoogleMutation } from 'src/redux/services/authApi';
 
-import { ButtonContainer, GoogleButton } from './styles';
+import { ButtonContainer } from './styles';
 
 export function GoogleLoginButton() {
   const [addUser, { data, isSuccess, isError, error }] =
     useAddUserGoogleMutation();
   const dispatch = useDispatch();
+
   const navigate = useNavigate();
-  const { t } = useTranslation();
 
   useEffect(() => {
     if (isSuccess) {
@@ -30,17 +29,17 @@ export function GoogleLoginButton() {
     }
   }, [data, dispatch, error, isError, isSuccess, navigate]);
 
-  const login = useGoogleLogin({
-    onSuccess: async credentialResponse => {
-      await addUser(credentialResponse);
-    },
-  });
-
   return (
     <ButtonContainer>
-      <GoogleButton onClick={() => login()}>
-        {t('loginPage.google')}
-      </GoogleButton>
+      <GoogleLogin
+        onSuccess={async credentialResponse => {
+          try {
+            await addUser(credentialResponse);
+          } catch (err) {
+            alert(err);
+          }
+        }}
+      />
     </ButtonContainer>
   );
 }
